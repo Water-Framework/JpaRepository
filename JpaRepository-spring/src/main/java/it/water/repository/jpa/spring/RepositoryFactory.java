df825d98a75530c65d0148a585f3404ff4438f8b
Aristide <aristide.cittadino@acsoftware.it>
2024-02-07 18:10:02 +0100

    adding logic more similar to what spring does on repository factory

--- JpaRepository-spring/src/main/java/it/water/repository/jpa/spring/RepositoryFactory.java
+++ JpaRepository-spring/src/main/java/it/water/repository/jpa/spring/RepositoryFactory.java
@@ -17,11 +17,18 @@
 
 package it.water.repository.jpa.spring;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.EscapeCharacter;
+import org.springframework.data.jpa.repository.query.JpaQueryMethodFactory;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
+import org.springframework.data.querydsl.EntityPathResolver;
+import org.springframework.data.querydsl.SimpleEntityPathResolver;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.EntityManager;
 
@@ -33,7 +40,9 @@ import javax.persistence.EntityManager;
  * @Author Aristide Cittadino
  */
 public class RepositoryFactory<R extends JpaRepository<T, K>,T,K> extends JpaRepositoryFactoryBean<R, T, K> {
-
+    private EntityPathResolver entityPathResolver;
+    private EscapeCharacter escapeCharacter = EscapeCharacter.DEFAULT;
+    private JpaQueryMethodFactory queryMethodFactory;
 
     /**
      * Creates a new {@link JpaRepositoryFactoryBean} for the given repository interface.
@@ -46,7 +55,41 @@ public class RepositoryFactory<R extends JpaRepository<T, K>,T,K> extends JpaRep
 
     @Override
     protected RepositoryFactorySupport createRepositoryFactory(EntityManager entityManager) {
-        return new WaterJpaExecutorFactory(entityManager);
+        WaterJpaExecutorFactory factory = new WaterJpaExecutorFactory(entityManager);
+        factory.setEntityPathResolver(entityPathResolver);
+        factory.setEscapeCharacter(escapeCharacter);
+        if (queryMethodFactory != null) {
+            factory.setQueryMethodFactory(queryMethodFactory);
+        }
+        return factory;
+    }
+
+    @Override
+    @Autowired
+    public void setEntityPathResolver(ObjectProvider<EntityPathResolver> resolver) {
+        this.entityPathResolver = resolver.getIfAvailable(() -> SimpleEntityPathResolver.INSTANCE);
+    }
+
+    /**
+     * Configures the {@link JpaQueryMethodFactory} to be used. Will expect a canonical bean to be present but will
+     * fallback to {@link org.springframework.data.jpa.repository.query.DefaultJpaQueryMethodFactory} in case none is
+     * available.
+     *
+     * @param factory may be {@literal null}.
+     */
+    @Override
+    @Autowired
+    public void setQueryMethodFactory(@Nullable JpaQueryMethodFactory factory) {
+
+        if (factory != null) {
+            this.queryMethodFactory = factory;
+        }
+    }
+
+    @Override
+    public void setEscapeCharacter(char escapeCharacter) {
+
+        this.escapeCharacter = EscapeCharacter.of(escapeCharacter);
     }
 
     /**
